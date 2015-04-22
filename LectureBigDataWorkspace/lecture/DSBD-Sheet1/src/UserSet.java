eb1b0febb28e1fec350c72c024780d80db361ca7
maria.muszynska <muszyns@cs.uni-bonn.de>
2015-04-22 19:18:58 +0200

    test

---
+++ LectureBigDataWorkspace/lecture/DSBD-Sheet1/src/UserSet.java
@@ -0,0 +1,13 @@
+
+public class UserSet {
+
+	
+public void add(String username){
+	//add a user to a userset
+}
+
+public double distanceTo(UserSet other){
+	double jaccardDistance = 0.0;
+	return jaccardDistance;
+}
+}
